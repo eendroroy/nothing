4368a62a1be3626264015f2673a7e16452fa4cbb
indrajit <eendroroy@gmail.com>
2017-12-12 03:05:04 +0600

    update prompt

--- nothing.zsh
+++ nothing.zsh
@@ -1,8 +1,11 @@
 #!/usr/bin/env zsh
 
+NT_PROMPT_SYMBOL=❱
+
 function precmd(){
   autoload -U add-zsh-hook
   setopt prompt_subst
-  PROMPT='%(?.%F{blue}(%i%).%F{red}(%i%))%F{green}%B🡪%b%f  '
-  RPROMPT='%B%(?..%F{red}$?%f%b'
+  PROMPT='%F{gray}%i %f%(?.%F{green}${NT_PROMPT_SYMBOL}%f.%F{red}${NT_PROMPT_SYMBOL}%f) '
+  RPROMPT='%(?..%F{red}%B%S  $?  %s%b%f'
 }
+
