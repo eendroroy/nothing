2d12abfa82151fc2b6f53fd3a0e830b155634642
indrajit <eendroroy@gmail.com>
2017-12-24 08:49:21 +0600

    #1 option to hide exit code and history count

--- nothing.zsh
+++ nothing.zsh
@@ -5,7 +5,20 @@ NT_PROMPT_SYMBOL=❱
 function precmd(){
   autoload -U add-zsh-hook
   setopt prompt_subst
-  PROMPT='%F{gray}%i %f%(?.%F{green}${NT_PROMPT_SYMBOL}%f.%F{red}${NT_PROMPT_SYMBOL}%f) '
-  RPROMPT='%(?..%F{red}%B%S  $?  %s%b%f'
+
+  if [[ "$NT_HIDE_COUNT" == '1' ]]; then
+  	command_count='  '
+  else
+  	command_count='%F{gray}%i %f'
+  fi
+
+  PROMPT='${command_count}%(?.%F{green}${NT_PROMPT_SYMBOL}%f.%F{red}${NT_PROMPT_SYMBOL}%f) '
+
+  if [[ "$NT_HIDE_EXIT_CODE" == '1' ]]; then
+  	RPROMPT=''
+  else
+  	RPROMPT='%(?..%F{red}%B%S  $?  %s%b%f)'
+  fi
+  
 }
 
